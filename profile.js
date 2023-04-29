cce8d5848d6d9605406e811a94328be199a3b065
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-04-29 20:58:08 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -21,6 +21,7 @@ function loggedIn() {
 loggedIn();
 
 logoutBtn.addEventListener("click", () => {
+  confirm("Are you sure you want to logout ? ")
   window.location.href = "./index.html";
   localStorage.removeItem("user");
 });
